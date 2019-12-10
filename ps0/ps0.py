d527976d2a88e8cdb9cdfe705ebc3698c44d87d4
Oscar0325 <yangjunfeng0325@163.com>
2019-12-10 21:20:17 +0800

    作业一

---
+++ ps0/ps0.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 10 20:30:49 2019
+
+@author: Lenovo
+"""
+import numpy as np
+x = int(input("Enter number x:"))
+y = int(input("Enter number y:"))
+print("X**y = {:d}".format(x**y))
+print("log(x) = {:.0f}".format(np.log2(x)))
